a7cd2e4d74868dc9450b8bb01f963d3cf4fe8db9
Michel Fortes <michel.fortes@softplan.com.br>
2024-07-18 01:46:45 -0300

    add more properties on owner

--- apps/veterinary_clinic/owners/pkg/model.go
+++ apps/veterinary_clinic/owners/pkg/model.go
@@ -1,6 +1,10 @@
 package pkg
 
 type Owner struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+	Id           string `json:"id"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName,omitempty"`
+	ProfileImage string `json:"profileImage,omitempty"`
+	Address      string `json:"address,omitempty"`
+	Phone        string `json:"phone,omitempty"`
 }
--- apps/veterinary_clinic/owners/repository/populator.go
+++ apps/veterinary_clinic/owners/repository/populator.go
@@ -3,7 +3,31 @@ package repository
 import "vet/owners/pkg"
 
 func populate() {
-	Create(pkg.Owner{Id: "65464199-544d-41b4-94a5-91d3099a3551", Name: "Carlos Augusto"})
-	Create(pkg.Owner{Id: "1dbcec23-0efd-442f-a6d9-54523a69f52b", Name: "Amanda"})
-	Create(pkg.Owner{Id: "f5b1e895-a853-4dd4-869f-d2d38f4a72c1", Name: "Gustavo"})
+
+	Create(pkg.Owner{
+		Id:           "65464199-544d-41b4-94a5-91d3099a3551",
+		FirstName:    "Carlos",
+		LastName:     "Augusto Barbosa",
+		Phone:        "(22) 2222 2222",
+		Address:      "Rua Xxxxx, xxxxx, xxxxxx",
+		ProfileImage: "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+	})
+
+	Create(pkg.Owner{
+		Id:           "1dbcec23-0efd-442f-a6d9-54523a69f52b",
+		FirstName:    "Amanda",
+		LastName:     "Santos da Silva",
+		Phone:        "(11) 1111 1111",
+		Address:      "Rua Zzzzz, zzzzzz, zzzzzzz",
+		ProfileImage: "https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png",
+	})
+
+	Create(pkg.Owner{
+		Id:        "f5b1e895-a853-4dd4-869f-d2d38f4a72c1",
+		FirstName: "Pedro",
+		LastName:  "Cortes Fonseca",
+		Phone:     "(55) 5555 5555",
+		Address:   "Rua Yyyyy, yyyyy, yyyyyy",
+	})
+
 }
